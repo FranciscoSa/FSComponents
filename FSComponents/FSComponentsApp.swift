79eb1d9c7d0f4f116ed02d3f33b836c09c5c6fa0
Francisco Santana Cardoso <fsantanagy@gmail.com>
2023-01-12 18:54:09 -0300

    start project

---
+++ FSComponents/FSComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FSComponentsApp.swift
+//  FSComponents
+//
+//  Created by Francisco Santana Cardoso on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FSComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
